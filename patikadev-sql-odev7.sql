946c2919678e0aab404c53989a2ddb26c479ea7c
sezen berre çelikoğlu <99209006+sezennnberre@users.noreply.github.com>
2023-02-23 17:50:19 +0300

    www.patika.dev

---
+++ patikadev-sql-odev7.sql
@@ -0,0 +1,13 @@
+SELECT rating FROM film
+GROUP BY rating;
+
+SELECT replacement_cost,COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+SELECT store_id,COUNT(*) FROM customer
+GROUP BY store_id;
+
+
+SELECT country_id,COUNT(country_id) FROM city
+GROUP BY country_id ORDER BY COUNT(country_id) DESC LIMIT 1;
